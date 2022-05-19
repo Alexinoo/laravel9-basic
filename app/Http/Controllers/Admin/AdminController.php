7ec77be9cbe1f57024bbc82e9a6f4f5c460d3f5c
Alex Mwangi <alex@stl-horizon.com>
2022-05-19 14:46:11 +0300

    Added Toastr notification after user loggs out

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -82,6 +82,11 @@ class AdminController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        $notification = array(
+            'message' => 'User Logout successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
     }
 }
