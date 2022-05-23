24f34c801606db0e95e6a00761ee2615dad43cf3
Alex Mwangi <alex@stl-horizon.com>
2022-05-23 22:08:12 +0300

    Added view to upload multiple images

---
+++ resources/views/Admin/About/uploads.blade.php
@@ -0,0 +1,66 @@
+@extends('Admin.admin_master')
+
+@section('content')
+
+<div class="page-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h4 class="card-title mb-5">Upload Multiple Images</h4>
+
+                        <form action="{{ route('upload.images') }}" method="POST" enctype="multipart/form-data">
+
+                            @csrf
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="col-sm-2 col-form-label">Select Images</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="file" name="images[]" id="multi_images" multiple />
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="col-sm-2 col-form-label"></label>
+                                <div class="col-sm-10">
+                                    <img class="rounded avatar-lg" src="{{ asset('upload/no_image.jpg')}}" alt="About Image" id="show_image" />
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <input type="submit" value="Upload Images" class="btn btn-info waves-effect waves-light" />
+                        </form>
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        $('#multi_images').change(function(e) {
+
+
+            var reader = new FileReader()
+
+            reader.onload = function(e) {
+                $('#show_image').attr('src', e.target.result)
+
+            }
+            reader.readAsDataURL(e.target.files['0'])
+        })
+
+
+    })
+
+</script>
+
+@endsection
