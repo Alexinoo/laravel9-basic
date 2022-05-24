7dab8471f3a5ec4ae2abb88d60d4e04f40dac72a
Alex Mwangi <alex@stl-horizon.com>
2022-05-24 23:12:27 +0300

    Added Blog CRUD views

---
+++ resources/views/Admin/Blog/edit.blade.php
@@ -0,0 +1,121 @@
+@extends('Admin.admin_master')
+
+@section('content')
+
+<style type="text/css">
+    .bootstrap-tagsinput .tag {
+        margin-right: 2px;
+        color: #b70000;
+        font-weight: 700px;
+    }
+
+</style>
+
+
+<div class="page-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h4 class="card-title">Edit Blog Details</h4>
+
+                        <hr style="width: 100%;border:1px solid #ddd;">
+
+                        <form action="{{ route('update.blog',$model->id) }}" method="POST" enctype="multipart/form-data">
+
+                            @csrf
+                            <div class="row mb-3">
+                                <label for="blog_category_id" class="col-sm-2 col-form-label"> Blog Category</label>
+                                <div class="col-sm-10">
+                                    <select name="blog_category_id" id="blog_category_id" class="form-select">
+                                        <option value="" selected="" disabled>--select category--</option>
+
+                                        @foreach($blog_categories as $key => $category)
+                                        <option value="{{$category->id}}" {{ ($model->blog_category_id == $category->id ) ?'selected':'' }}> {{$category->category}} </option>
+
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+
+                            <div class="row mb-3">
+                                <label for="blog_title" class="col-sm-2 col-form-label">Blog Title</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="text" name="blog_title" id="blog_title" value="{{$model->blog_title}}" />
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="example-text-input" class="col-sm-2 col-form-label">Blog Tags</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="text" name="blog_tags" value="{{$model->blog_tags}}" data-role="tagsinput">
+
+
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="elm1" class="col-sm-2 col-form-label"> Blog Description</label>
+                                <div class="col-sm-10">
+                                    <textarea id="elm1" name="blog_description">{!! $model->blog_description!!}</textarea>
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="blog_image" class="col-sm-2 col-form-label">Blog Image</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="file" name="blog_image" id="blog_image" />
+
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <div class="row mb-3">
+                                <label for="show_image" class="col-sm-2 col-form-label"></label>
+                                <div class="col-sm-10">
+                                    <img class="rounded avatar-lg" src="{{ !empty($model->blog_image) ? asset('upload/Blog_images/'.$model->blog_image) : asset('upload/no_image.jpg')}}" alt="Blog Image" id="show_image" />
+
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <input type="submit" value="Update Blog Data" class="btn btn-info waves-effect waves-light" />
+                        </form>
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        $('#blog_image').change(function(e) {
+
+            var reader = new FileReader()
+
+            reader.onload = function(e) {
+                $('#show_image').attr('src', e.target.result)
+
+            }
+            reader.readAsDataURL(e.target.files['0'])
+        })
+
+
+    })
+
+</script>
+
+@endsection
---
+++ resources/views/Admin/Blog/index.blade.php
@@ -0,0 +1,62 @@
+@extends('Admin.admin_master')
+
+@section('content')
+
+<div class="page-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h4 class="card-title mb-3">Blog 's</h4>
+                        <hr style="width: 100%;border:1px solid #ddd;margin-bottom:15px;">
+
+                        <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th>SL</th>
+                                    <th>Blog Category</th>
+                                    <th>Blog Title</th>
+                                    <th>Blog Image</th>
+                                    <th>Blog Tags</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php($i = 1)
+                                @foreach($model as $key => $value)
+                                <tr>
+                                    <td>{{$i++}}</td>
+                                    <td>{{$value->blog_category->category}}</td>
+
+                                    <td>{{$value->blog_title}}</td>
+                                    <td><img src="{{asset('upload/Blog_images/'.$value->blog_image)}}" alt="" style="width: 60px;height:50px;"></td>
+
+
+                                    <td>{{$value->blog_tags}}</td>
+
+                                    <td>
+                                        <div class="text-center">
+                                            <a href="{{ route('edit.blog',$value->id) }}" class="btn btn-info btn-rounded btn-sm"><i class="fa fa-pencil-alt" title="Edit Image"></i></a>
+
+
+                                            <a href="{{ route('delete.blog',$value->id) }}" class="btn btn-danger btn-rounded btn-sm ml-5" title="Delete Image" id="delete"><i class="fas fa-trash"></i></a>
+
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div> <!-- end row -->
+
+
+    </div>
+</div>
+@endsection
