0f73b351511791f68900c89a43d48c8d75b24cba
Alex Mwangi <alex@stl-horizon.com>
2022-05-24 20:03:45 +0300

    Added Portfolio page setup menu

--- resources/views/Admin/inc/sidebar.blade.php
+++ resources/views/Admin/inc/sidebar.blade.php
@@ -34,6 +34,20 @@
                </ul>
            </li>
 
+           <li>
+               <a href="javascript: void(0);" class="has-arrow waves-effect">
+                   <i class="ri-layout-3-line"></i>
+                   <span>Portfolio Setup</span>
+               </a>
+               <ul class="sub-menu" aria-expanded="false">
+                   <li><a href="{{route('portfolio.index')}}">All Portfolio</a></li>
+                   <li><a href="{{route('add.portfolio')}}">Add Portfolio </a></li>
+
+
+
+               </ul>
+           </li>
+
 
 
            <li class="menu-title">Pages</li>
