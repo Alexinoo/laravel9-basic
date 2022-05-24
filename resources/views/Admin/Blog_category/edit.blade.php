e0f7700401f95b8ce697f269bdbadf78895cc9fe
Alex Mwangi <alex@stl-horizon.com>
2022-05-24 21:24:10 +0300

    Added Blog category CRUD views

---
+++ resources/views/Admin/Blog_category/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('Admin.admin_master')
+
+@section('content')
+
+<div class="page-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <h4 class="card-title">Update Blog Category</h4>
+
+                        <hr style="width: 100%;border:1px solid #ddd;"><br><br>
+
+                        <form action="{{ route('update.category',$model->id) }}" method="POST">
+
+                            @csrf
+                            <div class="row mb-3">
+                                <label for="category" class="col-sm-2 col-form-label"> Blog Category</label>
+                                <div class="col-sm-10">
+                                    <input class="form-control" type="text" name="category" value="{{ $model->category }}" />
+                                    @error('category')
+                                    <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <!-- end row -->
+
+                            <input type="submit" value="Update Category" class="btn btn-info waves-effect waves-light" />
+                        </form>
+                    </div>
+                </div>
+            </div> <!-- end col -->
+        </div>
+
+    </div>
+</div>
+@endsection
