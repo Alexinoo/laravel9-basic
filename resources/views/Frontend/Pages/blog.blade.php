62c2ea564d4e7c3523a3aef54851ca217cfac7d5
Alex Mwangi <alex@stl-horizon.com>
2022-05-25 16:26:38 +0300

    Rendered latest Blog content dynamically - Showed date as DiffForHumans() format

--- resources/views/Frontend/Pages/blog.blade.php
+++ resources/views/Frontend/Pages/blog.blade.php
@@ -1,51 +1,35 @@
+@php
+
+$model = App\Models\Blog::latest()->limit(3)->get();
+
+@endphp
+
 <section class="blog">
     <div class="container">
         <div class="row gx-0 justify-content-center">
+
+            @foreach($model as $key => $value)
+
             <div class="col-lg-4 col-md-6 col-sm-9">
                 <div class="blog__post__item">
                     <div class="blog__post__thumb">
-                        <a href="blog-details.html"><img src="{{asset('frontend/assets/img/blog/blog_post_thumb01.jpg')}}" alt=""></a>
+                        <a href="blog-details.html"><img src="{{asset('upload/Blog_images/'.$value->blog_image)}}" alt=""></a>
                         <div class="blog__post__tags">
-                            <a href="blog.html">Story</a>
+                            <a href="blog.html">{{ $value->blog_category->category }}</a>
                         </div>
                     </div>
                     <div class="blog__post__content">
-                        <span class="date">13 january 2021</span>
-                        <h3 class="title"><a href="blog-details.html">Facebook design is dedicated to what's new in design</a></h3>
+                        <span class="date">{{ Carbon\Carbon::parse($value->created_at)->diffForHumans() }}</span>
+
+                        <h3 class="title"><a href="blog-details.html">{{$value->blog_title}}</a></h3>
                         <a href="blog-details.html" class="read__more">Read mORe</a>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-6 col-sm-9">
-                <div class="blog__post__item">
-                    <div class="blog__post__thumb">
-                        <a href="blog-details.html"><img src="{{asset('frontend/assets/img/blog/blog_post_thumb02.jpg')}}" alt=""></a>
-                        <div class="blog__post__tags">
-                            <a href="blog.html">Social</a>
-                        </div>
-                    </div>
-                    <div class="blog__post__content">
-                        <span class="date">13 january 2021</span>
-                        <h3 class="title"><a href="blog-details.html">Make communication Fast and Effectively.</a></h3>
-                        <a href="blog-details.html" class="read__more">Read mORe</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-9">
-                <div class="blog__post__item">
-                    <div class="blog__post__thumb">
-                        <a href="blog-details.html"><img src="{{asset('frontend/assets/img/blog/blog_post_thumb03.jpg')}}" alt=""></a>
-                        <div class="blog__post__tags">
-                            <a href="blog.html">Work</a>
-                        </div>
-                    </div>
-                    <div class="blog__post__content">
-                        <span class="date">13 january 2021</span>
-                        <h3 class="title"><a href="blog-details.html">How to increase your productivity at work - 2021</a></h3>
-                        <a href="blog-details.html" class="read__more">Read mORe</a>
-                    </div>
-                </div>
-            </div>
+
+            @endforeach
+
+
         </div>
         <div class="blog__button text-center">
             <a href="blog.html" class="btn">more blog</a>
