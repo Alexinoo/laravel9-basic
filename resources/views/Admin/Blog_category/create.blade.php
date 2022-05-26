35ebebadec4baeca1fe0a54ae9c0f290523f1bce
Alex Mwangi <alex@stl-horizon.com>
2022-05-26 11:39:18 +0300

    Added custom validation in action ; Added #myForm to form tag and form-group to input div

--- resources/views/Admin/Blog_category/create.blade.php
+++ resources/views/Admin/Blog_category/create.blade.php
@@ -13,16 +13,13 @@
 
                         <hr style="width: 100%;border:1px solid #ddd;"><br><br>
 
-                        <form action="{{ route('store.category') }}" method="POST">
+                        <form action="{{ route('store.category') }}" method="POST" id="myForm">
 
                             @csrf
                             <div class="row mb-3">
                                 <label for="category" class="col-sm-2 col-form-label"> Blog Category</label>
-                                <div class="col-sm-10">
+                                <div class=" form-group col-sm-10">
                                     <input class="form-control" type="text" name="category" />
-                                    @error('category')
-                                    <span class="text-danger">{{ $message }}</span>
-                                    @enderror
                                 </div>
                             </div>
                             <!-- end row -->
@@ -36,4 +33,43 @@
 
     </div>
 </div>
+@endsection
+
+{{-- Validate Input Fields --}}
+
+@section('scripts')
+<script>
+    $(document).ready(function() {
+
+        $('#myForm').validate({
+            rules: {
+                category: {
+                    required: true
+                } // Multiple fields use comma
+            }
+            , messages: {
+                category: {
+                    required: 'Please Enter Blog Category'
+                }
+            },
+
+            errorElement: 'span'
+            , errorPlacement: function(error, element) {
+                error.addClass('invalid-feedback')
+                element.closest('.form-group').append(error)
+
+            },
+
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('is-invalid')
+            }
+            , unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('is-invalid')
+            },
+
+        })
+    })
+
+</script>
+
 @endsection
