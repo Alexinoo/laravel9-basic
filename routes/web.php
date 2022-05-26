0359ea0c0814e6afce46286b899700cdb0214b66
Alex Mwangi <alex@stl-horizon.com>
2022-05-26 11:00:45 +0300

    Added name to (/) welcome page route

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('Frontend.dashboard');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('Admin.dashboard');
